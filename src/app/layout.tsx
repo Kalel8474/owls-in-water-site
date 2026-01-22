1460dd5e61b8790d765b656ab9e6d69f7085132f
Kalel8474 <scottmdavison@aol.com>
2026-01-21 18:25:41 -0600

    Added og-image for banners and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,30 @@ import { AudioProvider } from '@/components/AudioProvider'
 import MiniPlayer from '@/components/MiniPlayer'
 
 export const metadata: Metadata = {
-  title: 'Owls in Water | Music',
-  description: 'Official website of Owls in Water - Original music and stories',
+  title: 'Owls in Water',
+  description: 'Official website of Owls in Water - Original music',
   keywords: ['music', 'band', 'owls in water', 'original music'],
+  openGraph: {
+    title: 'Owls in Water',
+    description: 'Listen to Skyline Drive - the debut album from Owls in Water',
+    url: 'https://owlsinwater.com',
+    siteName: 'Owls in Water',
+    images: [
+      {
+        url: 'https://owlsinwater-media.s3.us-east-2.amazonaws.com/images/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Owls in Water',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Owls in Water',
+    description: 'Listen to Skyline Drive - the debut album from Owls in Water',
+    images: ['https://owlsinwater-media.s3.us-east-2.amazonaws.com/images/og-image.png'],
+  },
 }
 
 export default function RootLayout({
